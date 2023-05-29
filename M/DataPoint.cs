e31720d57d9693edf963b846615b24566dc63428
Miquel-TA <trazomobile4@gmail.com>
2023-05-29 11:19:38 +0200

    Added custom getters and setters to DataPoint to check and fix if doubles are incorrect (lost decimal separator)

--- M/DataPoint.cs
+++ M/DataPoint.cs
@@ -5,7 +5,38 @@ namespace ProfitCalculator
     public class DataPoint
     {
         public DateTime date { get; set; }
-        public double openingPrice { get; set; }
-        public double closingPrice { get; set; }
+        public double openingPrice {
+            get
+            {
+                return openingPrice;
+            }
+            set
+            {
+                if ((value.ToString().Contains(",") || value.ToString().Contains(".")) && value > 1000)
+                {
+                    openingPrice = value / 1000;
+                } else
+                {
+                    openingPrice = value;
+                }
+            }
+        }
+        public double closingPrice { 
+            get
+            {
+                return closingPrice;
+            }
+            set
+            {
+                if ((value.ToString().Contains(",") || value.ToString().Contains(".")) && value > 1000)
+                {
+                    closingPrice = value / 1000;
+                }
+                else
+                {
+                    closingPrice = value;
+                }
+            }
+        }
     }
 }
